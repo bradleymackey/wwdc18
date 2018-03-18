b939966b2b269e8dbaa01df152d6b44df91ddb5a
Bradley Mackey <bradleymackey98@googlemail.com>
2018-03-18 15:00:03 +0000

    created playground pages and links

---
+++ RSA.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+
+//: # RSA Encryption
+
+import UIKit
+import PlaygroundSupport
+
+var str = "Hello, playground"
+
+
+let view = UIView(frame: CGRect(x: 0, y: 0, width: 150, height: 150))
+view.backgroundColor = .red
+
+PlaygroundPage.current.liveView = view
+
+//: [Click Here to try it out!](@next)
--- RSA.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++
@@ -1,9 +0,0 @@
-//: Playground - noun: a place where people can play
-
-//: # Hello
-
-import UIKit
-
-var str = "Hello, playground"
-
-//: [Next](@next)
